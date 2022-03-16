db189349964e577b3ca6c2698aff9b12fed560cb
Valdemar-iatneh <avvakumov112003@gmail.com>
2022-03-16 10:38:54 +0300

    Add project files.

---
+++ TestProjectArray1/UnitTest1.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProjectArray1
+{
+    [TestClass]
+    public class UnitTestArray
+    {
+        [TestMethod]
+        public void TestGetLength()
+        {
+            int newLen = 4;
+            DynArray array = new DynArray(newLen);
+            int actLen = array.GetLength();
+            Assert.AreEqual(newLen, actLen, "[eq"); 
+        }
+    }
+
+    [TestClass]
+    public class UnitTestStack
+    {
+        [TestMethod]
+        public void TestIsEmpty()
+        {
+            MyStack stack = new MyStack();
+            MyStack stack1 = new MyStack();
+            stack1.Push(1);
+            stack1.Push(1);
+            stack1.Push(1);
+            Assert.IsTrue(stack1.IsEmpty(), "stack1 is not empty");
+            Assert.IsTrue(stack.IsEmpty(), "stack is not empty");
+        }
+    }
+}
